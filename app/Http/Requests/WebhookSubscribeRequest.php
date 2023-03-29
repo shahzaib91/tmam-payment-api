36b8df548d8b850e59be2e9cef4e6622b02a731a
Shahzaib <Shahzaib.2377@gmail.com>
2023-03-30 00:12:15 +0400

    Removed check to see if record already exists as it is covered in validation.

---
+++ app/Http/Requests/WebhookSubscribeRequest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+use Illuminate\Contracts\Validation\Validator;
+
+class WebhookSubscribeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'merchant_id' => 'required|numeric|exists:users,id',
+            'webhook_url' => 'required|url',
+            'webhook_secret' => 'required'
+        ];
+    }
+
+    /**
+     * Function to throw uniform validation error
+     *
+     * @param Validator $validator
+     * @throws HttpResponseException
+     * @return void
+     */
+    public function failedValidation(Validator $validator)
+    {
+
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'validation_error',
+            'data'      => $validator->errors(),
+            'code'      => 400
+        ]));
+    }
+
+    /**
+     * Function to customize messages
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'merchant_id.required' => 'Merchant ID field is required.',
+            'merchant_id.numeric' => 'Merchant ID must be a numeric value.',
+            'merchant_id.exists' => 'Merchant ID does not exists.',
+            'webhook_url.required' => 'Webhook url is required.',
+            'webhook_url.url' => 'Webhook url format is not valid.',
+            'webhook_secret.required' => 'Webhook secret salt is required.'
+        ];
+    }
+}
