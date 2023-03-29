af11265a05e70c0bbd93ebffbbc3d01123ab0347
Shahzaib <Shahzaib.2377@gmail.com>
2023-03-30 00:51:49 +0400

    Webhook sync function is removed from here and added into generic controller.

---
+++ app/Http/Controllers/WebhookController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\WebhookStateRequest;
+use App\Http\Requests\WebhookSubscribeRequest;
+use App\User;
+
+class WebhookController extends GenericController
+{
+    /**
+     * Function helps in subscribing webhook
+     *
+     * @param WebhookSubscribeRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function subscribe(WebhookSubscribeRequest $request)
+    {
+        $user = User::find($request->merchant_id);
+        $user->webhook_secret = $request->webhook_secret;
+        $user->webhook_url = $request->webhook_url;
+        $user->webhook_state = 'e'; // always set webhook state enabled when added
+        $user->save();
+
+        return $this->respondJson(200, 'success', true, $user);
+    }
+
+    /**
+     * Function helps in subscribing webhook
+     *
+     * @param WebhookStateRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function state(WebhookStateRequest $request)
+    {
+        $user = User::find($request->merchant_id);
+        $user->webhook_state = $request->webhook_state;
+        $user->save();
+        return $this->respondJson(200, 'success', true, $user);
+    }
+
+
+}
