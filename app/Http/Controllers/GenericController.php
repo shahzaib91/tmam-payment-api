6b7d2414216379215806285afac93f29e509e2a3
Shahzaib <Shahzaib.2377@gmail.com>
2023-03-30 13:42:36 +0400

    Finished web hook integration and finalized payment setup.

--- app/Http/Controllers/GenericController.php
+++ app/Http/Controllers/GenericController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Transaction;
+use GuzzleHttp\Client;
 
 class GenericController extends Controller
 {
@@ -29,7 +30,45 @@ class GenericController extends Controller
     protected function triggerWebhookEvent(Transaction $tx)
     {
         if($tx->merchant->webhook_state === 'e' && $tx->merchant->webhook_url != '') {
-            // ToDo with the webhook logic
+
+            // prepare payload
+            $payload = [
+                "transaction_id" => $tx->transaction_id,
+                "token" => $tx->token,
+                "transaction_type" => $tx->transaction_type,
+                "transaction_status" => $tx->transaction_status,
+                "merchant_code" => $tx->merchant_code,
+                "merchant_name" => $tx->merchant->name,
+                "merchant_country" =>$tx->merchant->country,
+                "merchant_currency" => $tx->merchant->currency,
+                "amount" => $tx->amount,
+                "transaction_currency" => $tx->transaction_currency,
+                "transaction_amount" => $tx->transaction_amount,
+                "auth_code" => $tx->auth_code,
+                "transaction_datetime" => $tx->created_at,
+            ];
+
+            // make and post to client
+            $client = new Client();
+            $response = $client->post(
+                $tx->merchant->webhook_url, [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        'Accept' => 'application/json',
+                        'X-Signature' => hash_hmac('sha256', json_encode($payload), $tx->merchant->webhook_secret),
+                    ],
+                    'json' => $payload
+                ]
+            );
+
+            // display response
+            // For debugging purpose only
+            dd(json_decode($response->getBody()));
+
+            // ToDo with the response here
+            // Following ways:
+            // 1- Parse and read response and set flag with the record
+            // 2- Logging webhook response data
         }
     }
 }
